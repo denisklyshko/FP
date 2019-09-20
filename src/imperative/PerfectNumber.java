befc54cf545ee906f769f66541191b2144b7cd54
unknown <denja320@gmail.com>
2019-09-20 17:03:58 +0300

    TASK 1-2

--- src/imperative/PerfectNumber.java
+++ src/imperative/PerfectNumber.java
@@ -1,12 +1,15 @@
 package imperative;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PerfectNumber {
 	
-	public static Set<Integer> divisors(int n) {
+	public static Set<Integer> divisors1(int n) {
 		
 		Set<Integer> res = new HashSet<>();
 		for (int i = 1; i <= n; i++) 
@@ -15,21 +18,45 @@ public class PerfectNumber {
 		return res;	
 	}
 	
+	public static Set<Integer> divisors(int n) {
+		Set<Integer> tmp =  IntStream.range(1, (int) Math.sqrt(n)+1)
+				.boxed()
+				.filter(i -> n % i == 0)
+				.collect(Collectors.toSet());
+		Set<Integer> res = new HashSet<>();
+		tmp.forEach((e) -> {
+			res.add(e);
+			if (n/e != e) res.add(n/e);
+		});
+		return res;
+	}
+		
 	public static STATE process(int n) {
-		int sum = 0;
-		for (int i = 1; i < n; i++) 
-			if (n % i == 0) 
-				sum = sum += i;
-		if      (sum < n)  return STATE.DEFICIENT;
-		else if (sum > n)  return STATE.ABUNDANT;
-		return STATE.PERFECT;
+		return STATE.fromId((IntStream.range(1, n)
+			.boxed()
+			.filter(i -> n % i == 0)
+			.reduce(0, Integer::sum)
+			.compareTo(n))); 
 	}
 	
-	public enum STATE {
-		PERFECT,
-		DEFICIENT,
-		ABUNDANT
-	}
 
+	public enum STATE {
+		PERFECT(0),
+		DEFICIENT(-1),
+		ABUNDANT(1);
+		
+		public final int num;
+		
+		STATE(int num) {
+			this.num = num;
+		}
+		
+        public static STATE fromId(int id) {
+            return Arrays.stream(STATE.values())
+            	.filter(e -> e.num == id)
+            	.findFirst()
+            	.orElse(null);
+        }	
+	}
 }
 
